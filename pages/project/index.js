18a066b3350b15dd5b109f6fc663902c1be27fa5
joseallef <46696111+joseallef@users.noreply.github.com>
2021-03-22 20:01:47 -0300

    fix(component): correção do erro da image undefined

--- pages/project/index.js
+++ pages/project/index.js
@@ -1,19 +1,29 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import ProjectCard from '../../src/components/ProjectCard';
 
-export default function Projects(
-  props,
-) {
+export default function Projects({ project }) {
+  const {
+    image, title, description, link,
+  } = project;
   return (
     <>
       <ProjectCard
-        image={props.project.image}
-        title={props.project.title}
-        description={props.project.description}
-        link={props.project.link}
+        image={image}
+        title={title}
+        description={description}
+        link={link}
       />
     </>
   );
 }
+Projects.defaultProps = {
+  project: {},
+};
+
+Projects.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  project: PropTypes.object,
+};
