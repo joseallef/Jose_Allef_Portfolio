9661fa1ff43eb6ee44bd7c3952ef0ff516e85702
joseallef <joseallefbs@gmail.com>
2024-06-01 23:57:55 -0300

    fix: ajuste na responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
-"use client";
-
 import { WrapperMain } from "@components/commons/Main";
-import { ThemeApp } from "@components/wrappers/context";
-import { useContext } from "react";
 
 export default function Home() {
-  const { themeApp } = useContext(ThemeApp);
   return (
     <WrapperMain>
       <div className="flex flex-col md:flex-row justify-center items-center h-full md:p-6">
         <div className="flex flex-col md:flex-row border border-sky-800 rounded-md">
-          <div className="flex flex-col p-2 md:p-3 justify-center items-center md:items-start w-full md:w-1/2">
+          <div className="flex flex-col p-2 md:p-3 justify-center md:justify-between items-center md:items-start w-full md:w-1/2">
             <div className="my-4 w-full">
               <h2 className="text-center w-full font-bold text-2xl md:text-3xl bg-gradient-to-r from-fuchsia-600 to-purple-800 bg-clip-text text-transparent">
                 Desenvolvedor Front-end
               </h2>
             </div>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full flex justify-center p-0 md:p-14 lg:p-0">
               <img
                 src="https://media.licdn.com/dms/image/D4D03AQEh-qNSzAsiJw/profile-displayphoto-shrink_400_400/0/1683594689756?e=1722470400&v=beta&t=llXFmcq_1qv2UtipcLuXu4wKtuebU6gMMn_VG5fRwHo"
                 alt="Foto de perfil"
-                className="object-cover lg:w-auto h-32 w-32 xl:h-64 xl:w-56 flex justify-center rounded-lg" />
+                className="object-cover sm:w-auto h-32 w-32 md:h-64 md:w-56 flex justify-center rounded-lg" />
             </div>
           </div>
 
